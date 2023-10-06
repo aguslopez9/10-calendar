caa524a044b0279ad9adb956e48e762f53ce791c
aguslopez9 <agustinlopezv99@gmail.com>
2023-10-06 20:04:29 -0300

    useform

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { uiSlice } from "./ui/uiSlice";
 import {calendarSlice} from './calendar/calendarSlice'
+import { authSlice } from "./auth/authSlice";
 
 
 
 
 export const store = configureStore({
     reducer: {
-        ui: uiSlice.reducer,
+        auth:     authSlice.reducer,
+        ui:       uiSlice.reducer,
         calendar: calendarSlice.reducer,
 
     },
